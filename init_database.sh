9208620ee3826b868b60a8bf7aa5929aac8e7c94
Lukáš Brzobohatý <brzda.l@gmail.com>
2018-09-12 16:23:25 +0200

    Fixed init database script

--- init_database.sh
+++ init_database.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-npx adonis migration:run
-npx adonis seed --files='ProjectExpModifierSeeder.js'
-npx adonis seed --files='ProjectSeeder.js'
-npx adonis seed --files='StandupProjectRatingEnumSeeder.js'
-npx adonis seed --files='StandupSeeder.js'
-npx adonis seed --files='UserSeeder.js'
+set -e
 
-npx adonis seed --files='ProjectMonthInstanceSeeder.js'
-npx adonis seed --files='StandupProjectRatingSeeder.js'
+npx adonis migration:run --force
+npx adonis seed --files='ProjectExpModifierSeeder.js' --force
+npx adonis seed --files='ProjectSeeder.js' --force
+npx adonis seed --files='StandupProjectRatingEnumSeeder.js' --force
+npx adonis seed --files='StandupSeeder.js' --force
+npx adonis seed --files='UserSeeder.js' --force
+
+npx adonis seed --files='ProjectMonthInstanceSeeder.js' --force
+npx adonis seed --files='StandupProjectRatingSeeder.js' --force
