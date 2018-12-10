f7a26f6244f60f11e3d0cda952a75f7a2a48702f
Vladimír Světlík <ladas215@gmail.com>
2018-12-10 18:22:37 +0100

    Improved note list layout

--- resources/components/NoteList.vue
+++ resources/components/NoteList.vue
@@ -1,19 +1,16 @@
 <template>
-  <v-layout row wrap>
-    <v-flex v-for="(item, index) in notes" :key="index" md2 class="ma-3">
-      <v-card color="brown lighten-5">
-        <v-card-text>
-          <div>
-            <v-layout reverse row justify-space-between>
-              <v-icon @click="() => markNoteCompleted(item.id)" color="green">done</v-icon>
-              <h2>{{ item.projectCode }}</h2>
-            </v-layout>
-            <div class="note-text">{{ item.text }}</div>
-          </div>
-        </v-card-text>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <div class="note-columns mx-3">
+    <v-card v-for="(item, index) in notes" :key="index" color="brown lighten-5" class="note-card mt-3">
+      <v-card-text>
+        <v-layout row nowrap>
+          <h2>{{ item.projectCode }}</h2>
+          <v-spacer></v-spacer>
+          <v-icon @click="markNoteCompleted(item.id)" color="green">done</v-icon>
+        </v-layout>
+        <div class="note-text">{{ item.text }}</div>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -41,11 +38,16 @@ export default {
 </script>
 
 <style scoped>
-.card {
-  width: 20%;
-}
-
 .note-text {
   white-space: pre-wrap;
+}
+.note-columns {
+  column-count: 6;
+  column-gap: 20px;
+  column-fill: balance;
+}
+.note-card {
+  display: inline-block;
+  width: 100%;
 }
 </style>
